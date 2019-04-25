a0d2658aa49b964ce073482a106058196b3d0aba
Shoichiro.kitano <ohmybigcatkotarou@gmail.com>
2019-04-25 15:22:14 +0900

    e2e用のヘルパーを作成

--- test/db_setup_test.go
+++ test/db_setup_test.go
@@ -2,21 +2,11 @@ package test
 
 import(
 	"testing"
-	"github.com/stretchr/testify/assert"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+
+	. "github.com/ShoichiroKitano/micro_test/test/helper"
 
 	"github.com/ShoichiroKitano/micro_test/db"
-	"github.com/ShoichiroKitano/micro_test/db/infra"
 )
-
-func TruncateTable(rdbms, connectionInformation, tableName string) {
-	infra.FindDBConnection(rdbms, connectionInformation).TruncateTable(tableName)
-}
-
-func FindDriver(rdbms, connectionInformation string) *sql.DB {
-	return infra.FindDBConnection(rdbms, connectionInformation).Driver
-}
 
 func TestDBはデータのセットアップができる(t *testing.T) {
 	defer TruncateTable("mysql", "root:@/test_micro_test", "test")
@@ -29,7 +19,7 @@ func TestDBはデータのセットアップができる(t *testing.T) {
 		Record("B1", "B2"),
 	)
 
-	rows, _ := FindDriver("mysql", "root:@/test_micro_test").Query("SELECT * FROM test;")
+	rows := Select("mysql", "root:@/test_micro_test", "test")
 	defer rows.Close()
 	AssertNextRow(t, rows, "A1", "A2")
 	AssertNextRow(t, rows, "B1", "B2")
@@ -53,21 +43,10 @@ func TestDBはデフォルト値をつかってデータのセットアップが
 		Record("B1"),
 	)
 
-	rows, _ := FindDriver("mysql", "root:@/test_micro_test").Query("SELECT * FROM test;")
+	rows := Select("mysql", "root:@/test_micro_test", "test")
 	defer rows.Close()
 	AssertNextRow(t, rows, "A1", "default2")
 	AssertNextRow(t, rows, "B1", "default2")
 	AssertNextIsNone(t, rows)
 }
 
-func AssertNextRow(t *testing.T, rows *sql.Rows, columnValue1, columnValue2 string) {
-	var column1, column2 string
-	assert.True(t, rows.Next(), "fail next")
-	rows.Scan(&column1, &column2)
-	assert.Equal(t, columnValue1, column1, "fail column1")
-	assert.Equal(t, columnValue2, column2, "fail column1")
-}
-
-func AssertNextIsNone(t *testing.T, rows *sql.Rows) {
-	assert.False(t, rows.Next())
-}
