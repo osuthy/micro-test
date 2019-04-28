d1813c2e89600ed21a2cf0e94b00b66fd1942123
Shoichiro.kitano <ohmybigcatkotarou@gmail.com>
2019-04-28 15:10:26 +0900

    テストの説明をブロックでできるようにする

--- dsl/dsl.go
+++ dsl/dsl.go
@@ -4,6 +4,10 @@ import (
 	"github.com/ShoichiroKitano/micro_test/runner"
 )
 
+func Feature(description string, testFunction func()) {
+	testFunction()
+}
+
 func Test(description string, testFunction func()) interface{} {
 	runner.TestFunctions = append(runner.TestFunctions, testFunction)
 	return nil
--- test/test_execution_test.go
+++ test/test_execution_test.go
@@ -11,12 +11,17 @@ import(
 
 func Test定義したテストを実行できる(t *testing.T) {
 	result1 := 0
-	dsl.Test("test dscription1", func(){
-		result1++
-	})
 	result2 := 0
-	dsl.Test("test dscription2", func(){
-		result2++
+	dsl.Feature("feature description", func() {
+		dsl.Feature("sub feature description", func() {
+			dsl.Test("test dscription1", func(){
+				result1++
+			})
+
+			dsl.Test("test dscription2", func(){
+				result2++
+			})
+		})
 	})
 
 	runner.Run()
