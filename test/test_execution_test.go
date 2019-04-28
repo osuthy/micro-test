b6d83590991a4200dd43ce51fb68fd8635a2efbd
Shoichiro.kitano <ohmybigcatkotarou@gmail.com>
2019-04-28 22:17:19 +0900

    テスト用のDSLを作成

--- test/test_execution_test.go
+++ test/test_execution_test.go
@@ -13,14 +13,12 @@ func Test定義したテストを実行できる(t *testing.T) {
 	result1 := 0
 	result2 := 0
 	dsl.Feature("feature description", func() {
-		dsl.Feature("sub feature description", func() {
-			dsl.Test("test dscription1", func(){
-				result1++
-			})
+		dsl.Test("test dscription1", func(){
+			result1++
+		})
 
-			dsl.Test("test dscription2", func(){
-				result2++
-			})
+		dsl.Test("test dscription2", func(){
+			result2++
 		})
 	})
 
@@ -28,4 +26,44 @@ func Test定義したテストを実行できる(t *testing.T) {
 
 	assert.Equal(t, 1, result1)
 	assert.Equal(t, 1, result2)
+}
+
+func TestImplicitSetUpをテスト毎に行える(t *testing.T) {
+	results := []string{}
+	dsl.Feature("feature description", func() {
+		dsl.Before(func() {
+			results = append(results, "setUp")
+		})
+
+		dsl.Test("test dscription1", func(){
+			results = append(results, "description1")
+		})
+
+		dsl.Test("test dscription2", func(){
+			results = append(results, "description2")
+		})
+	})
+
+	runner.Run()
+
+	assert.Equal(t, []string{"setUp", "description1", "setUp", "description2"}, results)
+}
+
+func Testトップレベルの宣言ごとにテストSuiteが構築される(t *testing.T) {
+	results := []string{}
+	dsl.Feature("feature description1", func() {
+		dsl.Test("test dscription1", func(){
+			results = append(results, "description1")
+		})
+	})
+
+	dsl.Feature("feature description2", func() {
+		dsl.Test("test dscription2", func(){
+			results = append(results, "description2")
+		})
+	})
+
+	runner.Run()
+
+	assert.Equal(t, []string{"description1", "description2"}, results)
 }
