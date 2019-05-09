a92674d50275fbe8ea09e615434eb9e4fbc341a9
Yuki.kakegwa <yuki.kakegwa@yuki.local>
2019-05-09 21:30:24 +0900

    任意のオブジェクトからJSONに変換する処理を実装

---
+++ json/json.go
@@ -0,0 +1,27 @@
+package json
+
+import(
+	"encoding/json"
+)
+
+type O map[string]interface{}
+
+func (this O) Print() {
+  println("O")
+}
+
+func (this O) ToJson() []byte {
+	json, _ := json.Marshal(this)
+	return json
+}
+
+type A []interface{}
+
+func (this A) Print() {
+  println("A")
+}
+
+func (this A) ToJson() []byte {
+	json, _ := json.Marshal(this)
+	return json
+}
