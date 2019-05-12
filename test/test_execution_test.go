d5e7c0d7d4cdfb5abcad67abf6f921a14285ee6f
Shoichiro.kitano <ohmybigcatkotarou@gmail.com>
2019-05-12 20:58:38 +0900

    検証方法を変更

--- test/test_execution_test.go
+++ test/test_execution_test.go
@@ -7,33 +7,31 @@ import(
 
 	"github.com/ShoichiroKitano/micro_test"
 	"github.com/ShoichiroKitano/micro_test/runner"
-	. "github.com/ShoichiroKitano/micro_test/testable"
+	"github.com/ShoichiroKitano/micro_test/testable"
 )
 
 func resetSuites() {
-	micro_test.Suites = []Testable{}
+	micro_test.Suites = []testable.Testable{}
 }
 
 func Test定義したテストを実行できる(t *testing.T) {
 	defer resetSuites()
 	resetSuites()
 
-	result1 := 0
-	result2 := 0
+	results := []string{}
 	micro_test.Feature("feature description", func() {
 		micro_test.Test("test dscription1", func(){
-			result1++
+			results = append(results, "test1")
 		})
 
 		micro_test.Test("test dscription2", func(){
-			result2++
+			results = append(results, "test2")
 		})
 	})
 
 	runner.Run()
 
-	assert.Equal(t, 1, result1)
-	assert.Equal(t, 1, result2)
+	assert.Equal(t, []string{"test1", "test2"}, results)
 }
 
 func TestImplicitSetUpをテスト毎に行える(t *testing.T) {
@@ -73,17 +71,17 @@ func Testトップレベルの宣言ごとにテストSuiteが構築される(t 
 	results := []string{}
 	micro_test.Feature("feature description1", func() {
 		micro_test.Test("test dscription1", func(){
-			results = append(results, "description1")
+			results = append(results, "test1")
 		})
 	})
 
 	micro_test.Feature("feature description2", func() {
 		micro_test.Test("test dscription2", func(){
-			results = append(results, "description2")
+			results = append(results, "test2")
 		})
 	})
 
 	runner.Run()
 
-	assert.Equal(t, []string{"description1", "description2"}, results)
+	assert.Equal(t, []string{"test1", "test2"}, results)
 }
