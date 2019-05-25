01c22d8ae6691c0882c0e7f9b6b703fad3b5d0c7
Shoichiro.kitano <ohmybigcatkotarou@gmail.com>
2019-05-25 10:27:22 +0900

    wiremockをテストコードがスタブできるように変更

--- test/http_request_test.go
+++ test/http_request_test.go
@@ -3,11 +3,17 @@ package test
 import(
 	"testing"
 	"github.com/stretchr/testify/assert"
+
 	"github.com/ShoichiroKitano/micro_test/http"
 	"github.com/ShoichiroKitano/micro_test/json"
+
+	"github.com/ShoichiroKitano/micro_test/test/wiremock"
 )
 
 func TestHttpはサーバーにメソッドGETでリクエストを送ることができる(t *testing.T) {
+	defer wiremock.Reset("localhost:8080")
+	wiremock.Stubbing("localhost:8080", "/test", "GET",
+      `{ \"object\": \"value\", \"array\": [1, 2, 3] }`, 200, "test success")
 	var body string
 	var status int
 	status, body = http.Server("localhost:8080").
--- test/wiremock/wiremock.go
+++ test/wiremock/wiremock.go
@@ -2,6 +2,8 @@ package wiremock
 
 import (
 	"net/http"
+	"bytes"
+	"strconv"
 )
 
 func Reset(url string) {
@@ -9,3 +11,23 @@ func Reset(url string) {
 	http.DefaultClient.Do(req)
 }
 
+func Stubbing(url string, path string, method string, json string, status int, body string) {
+	req, _ := http.NewRequest("POST", "http://" + url +"/__admin/mappings/new",
+	bytes.NewBuffer([]byte(`
+		{
+			"request":
+				{
+					"url": "`+ path + `",
+					"method": "` + method + `",
+					"bodyPatterns": [{
+						"equalToJson": "` + json + `"
+					}]
+				},
+			"response":
+				{
+					"status": ` + strconv.Itoa(status) + `,
+					"body": "` + body + `"
+				}
+		}`)))
+	http.DefaultClient.Do(req)
+}
