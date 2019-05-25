ae6266229755f4cb5ee2790ffe054189edd2dc82
Shoichiro.kitano <ohmybigcatkotarou@gmail.com>
2019-05-25 11:06:29 +0900

    配列がトップレベルに来るJSONを送るパターンを追加

--- test/http_request_test.go
+++ test/http_request_test.go
@@ -9,18 +9,25 @@ import(
 
 	"github.com/ShoichiroKitano/micro_test/test/wiremock"
 )
-
 func TestHttpはサーバーにJSONを送ることができる(t *testing.T) {
+	wiremock.Reset("localhost:8080")
+
 	t.Run("Objectがトップレベルの階層の場合", func(t *testing.T) {
 		defer wiremock.Reset("localhost:8080")
 		wiremock.Stubbing("localhost:8080", "/test", "GET",
-		`{ \"object\": \"value\", \"array\": [1, 2, 3] }`, 200, "test success")
+		`{ \"object\": \"value\" }`, 200, "test success")
 		status, body := http.Server("localhost:8080").
-		ReceiveRequest("GET", "/test",
-		http.WithJson(json.O{
-			"object": "value",
-			"array": json.A{1, 2, 3},
-		}))
+		ReceiveRequest("GET", "/test", http.WithJson(json.O{"object": "value"}))
+		assert.Equal(t, 200, status)
+		assert.Equal(t, "test success", body)
+	})
+
+	t.Run("Arrayがトップレベルの階層の場合", func(t *testing.T) {
+		defer wiremock.Reset("localhost:8080")
+		wiremock.Stubbing("localhost:8080", "/test", "GET",
+		`[1, 2, 3]`, 200, "test success")
+		status, body := http.Server("localhost:8080").
+		ReceiveRequest("GET", "/test", http.WithJson(json.A{1, 2, 3}))
 		assert.Equal(t, 200, status)
 		assert.Equal(t, "test success", body)
 	})
