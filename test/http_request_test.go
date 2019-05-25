5f9857933339fae94e4a221c04050fc54b795127
Shoichiro.kitano <ohmybigcatkotarou@gmail.com>
2019-05-25 10:35:23 +0900

    テストコードを階層で説明するように変更

--- test/http_request_test.go
+++ test/http_request_test.go
@@ -10,18 +10,18 @@ import(
 	"github.com/ShoichiroKitano/micro_test/test/wiremock"
 )
 
-func TestHttpはサーバーにメソッドGETでリクエストを送ることができる(t *testing.T) {
-	defer wiremock.Reset("localhost:8080")
-	wiremock.Stubbing("localhost:8080", "/test", "GET",
-      `{ \"object\": \"value\", \"array\": [1, 2, 3] }`, 200, "test success")
-	var body string
-	var status int
-	status, body = http.Server("localhost:8080").
-	ReceiveRequest("GET", "/test",
-	http.WithJson(json.O{
-		"object": "value",
-		"array": json.A{1, 2, 3},
-	}))
-	assert.Equal(t, 200, status)
-	assert.Equal(t, "test success", body)
+func TestHttpはサーバーにJSONを送ることができる(t *testing.T) {
+	t.Run("Objectがトップレベルの階層の場合", func(t *testing.T) {
+		defer wiremock.Reset("localhost:8080")
+		wiremock.Stubbing("localhost:8080", "/test", "GET",
+		`{ \"object\": \"value\", \"array\": [1, 2, 3] }`, 200, "test success")
+		status, body := http.Server("localhost:8080").
+		ReceiveRequest("GET", "/test",
+		http.WithJson(json.O{
+			"object": "value",
+			"array": json.A{1, 2, 3},
+		}))
+		assert.Equal(t, 200, status)
+		assert.Equal(t, "test success", body)
+	})
 }
