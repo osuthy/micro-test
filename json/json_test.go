d2318ac8a426f059e0d68dda10cb1f3e7b657194
Yuki Kakegawa <fasxyuqi@gmail.com>
2019-07-16 22:10:23 +0900

    リファクタリング

--- json/json_test.go
+++ json/json_test.go
@@ -7,38 +7,32 @@ import (
 
 func Test各要素をJSONに変換する(t *testing.T) {
 	t.Run("文字列の場合", func(t *testing.T) {
-		var obj Object = A{"aho"}
-		actual := obj.ToJson()
+		actual := A{"aho"}.ToJson()
 		assert.Equal(t, `["aho"]`, string(actual))
 	})
 
 	t.Run("文字列の配列の場合", func(t *testing.T) {
-		a := A{"dog", "bird", "cat"}
-		actual := a.ToJson()
+		actual := A{"dog", "bird", "cat"}.ToJson()
 		assert.Equal(t, `["dog","bird","cat"]`, string(actual))
 	})
 
 	t.Run("整数の場合", func(t *testing.T) {
-		a := A{100}
-		actual := a.ToJson()
+		actual := A{100}.ToJson()
 		assert.Equal(t, `[100]`, string(actual))
 	})
 
 	t.Run("整数の配列の場合", func(t *testing.T) {
-		a := A{1, 2, 3}
-		actual := a.ToJson()
+		actual := A{1, 2, 3}.ToJson()
 		assert.Equal(t, `[1,2,3]`, string(actual))
 	})
 
 	t.Run("入れ子構造のオブジェクトの場合", func(t *testing.T) {
-		o := O{"string": "str", "object": O{"nestedObject": O{"int": 100}}}
-		actual := o.ToJson()
+		actual := O{"string": "str", "object": O{"nestedObject": O{"int": 100}}}.ToJson()
 		assert.Equal(t, `{"object":{"nestedObject":{"int":100}},"string":"str"}`, string(actual))
 	})
 
 	t.Run("配列を持つ入れ子構造のオブジェクトの場合", func(t *testing.T) {
-		o := O{"string": "str", "intArrays": A{O{"one": 1}, O{"two": 2}}}
-		actual := o.ToJson()
+		actual := O{"string": "str", "intArrays": A{O{"one": 1}, O{"two": 2}}}.ToJson()
 		assert.Equal(t, `{"intArrays":[{"one":1},{"two":2}],"string":"str"}`, string(actual))
 	})
 
@@ -46,28 +40,24 @@ func Test各要素をJSONに変換する(t *testing.T) {
 
 func TestJsonの要素を上書きする(t *testing.T) {
 	t.Run("要素がオブジェクトの場合", func(t *testing.T) {
-		o := O{"key1": "value1", "key2": "value2"}
-		actual := o.Override("key1", "value100")
+		actual := O{"key1": "value1", "key2": "value2"}.Override("key1", "value100")
 		assert.Equal(t, O{"key1": "value100", "key2": "value2"}, actual)
 	})
 }
 
 func TestJsonの各要素をインクリメントして返却する(t *testing.T) {
 	t.Run("int型の場合", func(t *testing.T) {
-		o := O{"o": 10}
-		actual := o.Generate(2)
+		actual := O{"o": 10}.Generate(2)
 		assert.Equal(t, A{O{"o": 11}, O{"o": 12}}, actual)
 	})
 
 	t.Run("float64型の場合", func(t *testing.T) {
-		o := O{"o": 10.1}
-		actual := o.Generate(2)
+		actual := O{"o": 10.1}.Generate(2)
 		assert.Equal(t, A{O{"o": 11.1}, O{"o": 12.1}}, actual)
 	})
 
 	t.Run("T型の場合", func(t *testing.T) {
-		o := O{"o": T("2001-12-20 23:59:59")}
-		actual := o.Generate(2)
+		actual := O{"o": T("2001-12-20 23:59:59")}.Generate(2)
 		assert.Equal(t, A{O{"o": T("2001-12-20 23:59:59")}, O{"o": T("2001-12-20 23:59:59")}}, actual)
 	})
 }
