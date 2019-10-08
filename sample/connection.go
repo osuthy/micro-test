31f909b13804e622abb94ed4b506398e83a1d19b
Yuki Kakegawa <fasxyuqi@gmail.com>
2019-10-08 22:02:41 +0900

    create mysql connection

--- sample/connection.go
+++ sample/connection.go
@@ -3,8 +3,21 @@ package main
 import (
 	"fmt"
 	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	//[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	// root:micro-tes@tcp(127.0.0.1:3306)/sample
+	db, err := sql.Open("mysql", "root:micro-test@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = db.Ping()
+    if err != nil {
+        panic(err)
+	}
+
 	fmt.Println("Hello World.")
 }
