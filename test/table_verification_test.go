04dc0900386405b7e72a2e4f7e46c944bcaed811
ShoichiroKitano <ohmybigcatkotarou@gmail.com>
2019-12-05 22:08:28 +0900

    ファイルを結合

--- test/table_verification_test.go
+++ test/table_verification_test.go
@@ -1,8 +1,9 @@
 package test
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+	"database/sql"
+	"github.com/stretchr/testify/assert"
 
 	. "github.com/osuthy/micro-test/db/infra"
 
@@ -340,4 +341,26 @@ func Test事前条件のデータの補完(t *testing.T) {
 	assert.Equal(t, uint8(0x0), longblobc)
 
 	AssertNextIsNone(t, rows)
+}
+
+func TestDBは指定したテーブルをtruncateできる(t *testing.T) {
+	t.Skip()
+	defer TruncateTable("mysql", "root:@/test_micro_test", "test")
+
+	DB(TC{}, "conName").HasRecords(
+		Table("test").
+			Columns("column1", "column2").
+			Record("A1", "A2").
+			Record("B1", "B2"),
+	)
+
+	DB(TC{}, "conName").Truncate("test")
+
+	rows := Select("mysql", "root:@/test_micro_test", "test")
+	defer rows.Close()
+	AssertEmpty(t, rows)
+}
+
+func AssertEmpty(t *testing.T, rows *sql.Rows) {
+	assert.False(t, rows.Next())
 }
